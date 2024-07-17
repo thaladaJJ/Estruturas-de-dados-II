fdb13293d73db23569410b21b8a8833b2e58a4de
Thales Euler <thaleslevs@gmail.com>
2024-07-17 16:50:26 -0300

    base ex 5 percursos

---
+++ ex5-percursos/posorder.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main () {
+
+    int num_casos;
+    scanf ("%d", &num_casos);
+
+    for (int i = 0; i < num_casos; i++) {
+        
+        int num_nodes;
+        char preOrder_string [53];
+        char inOrder_string [53];
+        scanf ("%d %s %s", &num_nodes, preOrder_string, inOrder_string);
+
+           
+
+    }
+
+
+    return 0;
+}
